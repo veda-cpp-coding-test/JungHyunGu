cb45fd8d59039bbc41642cfcf11c64260c97b900
currentnine <wjdgusrn951@naver.com>
2025-05-27 12:20:12 +0900

    10week / BOJ11651.좌표 정렬하기 2 / 실버5 / 정렬1 / 시간 104ms / 메모리 2808KB

---
+++ 10week/11651.cpp
@@ -0,0 +1,29 @@
+//10week / BOJ11651.좌표 정렬하기 2 / 실버5 / 정렬1 / 시간 104ms / 메모리 2808KB
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(pair<int, int> a, pair<int, int> b )
+{
+    if(a.second==b.second) return a.first < b.first;
+    return a.second < b.second;
+}
+
+
+int main()
+{
+    
+    int n;
+    cin>>n;
+    vector <pair<int,int>> v(n);
+    for(int i=0;i<n;i++) 
+    {
+        cin>>v[i].first >> v[i].second;
+    }
+
+    sort(v.begin(), v.end(), compare);
+    for(int i = 0;i<n;i++) cout<< v[i].first << " " << v[i].second<<"\n";
+}
